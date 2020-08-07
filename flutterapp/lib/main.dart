207c35fef4908916da2030e02844ee933c9c2715
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-08-07 21:55:57 +0530

    image

--- flutterapp/lib/main.dart
+++ flutterapp/lib/main.dart
@@ -14,16 +14,8 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[700], 
       ),
       body: Center(
-        child: Text("Hello!!",
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower',
+        child: Image.network('https://images.unsplash.com/photo-1596610047311-dd6c235ac195?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80') 
         ),
-       ),
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed:() => {},
         child: Text('Click.', style: TextStyle(fontSize: 15.0),),
