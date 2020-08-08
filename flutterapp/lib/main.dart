864bc3af8d225c0ecc6e1add529654ae9d4f26ca
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-08-08 14:01:50 +0530

    id card

--- flutterapp/lib/main.dart
+++ flutterapp/lib/main.dart
@@ -1,48 +1,91 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Home()));
+  runApp(MaterialApp(home: NinjaCard()));
 }
-class Home extends StatelessWidget {
+
+class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text("My Second App"),
+        title: Text("Ninja   Id Card"),
         centerTitle: true,
-        backgroundColor: Colors.green[700], 
+        backgroundColor: Colors.green[700],
       ),
-
-      body: Row(
-        // mainAxisAlignment:MainAxisAlignment.spaceEvenly,
-        // crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Expanded(flex: 3,
-                      child: Container(
-              color:Colors.pink[300],
-              padding: EdgeInsets.all(20.0),
-              child: Text("1"),),
-          ),
-          Expanded(flex: 2,
-                      child: Container(
-              color:Colors.cyan,
-              padding: EdgeInsets.all(20.0),
-              child: Text("2"),),
-          ),
-          Expanded(flex: 1,
-                      child: Container(
-              color:Colors.green,
-              padding: EdgeInsets.all(20.0),
-              child: Text("3")),
-          )],     
-          ),
-        floatingActionButton: FloatingActionButton(
-        onPressed:() => {},
-        child: Text('Click.', style: TextStyle(fontSize: 15.0),),
+      body: Padding(
+          padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  child: Image.network(
+                      "https://images.unsplash.com/photo-1596610047311-dd6c235ac195?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"),
+                ),
+              ),
+              Text(
+                "NAME",
+                style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "Chun-Li",
+                style: TextStyle(
+                    color: Colors.amber,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                "CURRENT NINJA LEVEL",
+                style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "8",
+                style: TextStyle(
+                    color: Colors.amber,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.blue[200],
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'chun.li@ninja.com',
+                    style: TextStyle(color: Colors.grey[400], fontSize: 18),
+                  )
+                ],
+              )
+            ],
+          )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: Text(
+          'Click.',
+          style: TextStyle(fontSize: 15.0),
+        ),
         backgroundColor: Colors.red[700],
       ),
     );
   }
 }
-
